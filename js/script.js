9d22f1f7e8c130fda262868a51ff4ef70eb95e2c
Raju Ahmed <rajubdpro@gmail.com>
2024-12-19 16:16:04 +0600

    Updated code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,57 @@
-let buttons = document.querySelectorAll(".button");
-let body = document.querySelector("body");
+const body = document.querySelector('body')
+const buttons = document.querySelectorAll('.button')
+const btnarr = Array.from(buttons)
 
-buttons.forEach((item) => {
-  item.addEventListener("click", function (e) {
-    if (e.target.id === "grey") {
-      body.style.background = e.target.id;
+// Heading color
+const heading1 = document.querySelector('.heading1')
+const heading2 = document.querySelector('.heading2')
+const para1 = document.querySelector('.para1')
+const para2 = document.querySelector('.para2')
+
+btnarr.map(items=>{
+  items.addEventListener('click', function(e){
+    if(e.target.id=='red'){
+      body.style.background = 'red'
+        // Text color
+        heading1.style.color='#ffff'
+        heading2.style.color='#ffff'
+        para1.style.color='#ffff'
+        para2.style.color='#ffff'
     }
-    if (e.target.id === "green") {
-      body.style.background = e.target.id;
+    if(e.target.id=='green'){
+      body.style.background = 'green'
+              // Text color
+        heading1.style.color='#ffff'
+        heading2.style.color='#ffff'
+        para1.style.color='#ffff'
+        para2.style.color='#ffff'
     }
-    if (e.target.id === "yellow") {
-      body.style.background = e.target.id;
+    if(e.target.id=='purple'){
+      body.style.background = 'purple'
+              // Text color
+        heading1.style.color='#ffff'
+        heading2.style.color='#ffff'
+        para1.style.color='#ffff'
+        para2.style.color='#ffff'
     }
-    if (e.target.id === "purple") {
-      body.style.background = e.target.id;
+    if(e.target.id=='blue'){
+      body.style.background = 'blue'
+              // Text color
+        heading1.style.color='#ffff'
+        heading2.style.color='#ffff'
+        para1.style.color='#ffff'
+        para2.style.color='#ffff'
     }
-  });
-});
+    if(e.target.id=='yellow'){
+      body.style.background = 'yellow'
+              // Text color
+        heading1.style.color='#333'
+        heading2.style.color='#333'
+        para1.style.color='#333'
+        para2.style.color='#333'
+    }
+  })
+})
+
+
+
